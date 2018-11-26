c0e001c08e18bf199f81a252c0ccac65951c1ccc
Arthur.Chen <arthur.chen.china@gmail.com>
2018-11-26 19:58:03 +0000

    delete the package test

--- src/main/java/com/my/concurrency/test/TestForm.java
+++
@@ -1,9 +0,0 @@
-package com.my.concurrency.test;
-
-import com.my.concurrency.forms.MainForm;
-
-public class TestForm {
-    public static void main(String[] args) {
-        MainForm mainForm = new MainForm();
-    }
-}
